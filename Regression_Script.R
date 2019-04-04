db91c776b94e558149049fb01d7bb680735e2ede
Waggoner <jr@waggs.net>
2019-04-04 17:26:15 -0500

    playing around with some of the data

--- Regression_Script.R
+++ Regression_Script.R
@@ -1,4 +1,8 @@
+
 library(dplyr)
+library(ggplot2)
+library(readr)
+
 
 loandata <- read.csv('LoanStats3a.csv')
 
@@ -7,3 +11,14 @@ failed_loan <- loandata %>%
   select(loan_amnt,term,int_rate,installment,sub_grade,home_ownership,
          annual_inc,pymnt_plan,purpose,dti,total_pymnt,total_rec_prncp) %>%
   mutate(pct_paid = total_rec_prncp/loan_amnt * 100)
+
+ggplot(failed_loan, aes(loan_amnt,total_pymnt)) +
+  geom_point()
+
+ggplot(failed_loan, aes(loan_amnt,pct_paid)) +
+  geom_point()
+
+ggplot(failed_loan, aes(pct_paid))+
+  geom_histogram()
+
+pairs(failed_loan[1:5])
