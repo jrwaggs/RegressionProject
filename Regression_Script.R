c4478360a793050722702b3a48355377624ef0e3
Waggoner <jr@waggs.net>
2019-04-05 09:59:51 -0500

    added data dictionary, more work on script

--- Regression_Script.R
+++ Regression_Script.R
@@ -3,14 +3,34 @@ library(dplyr)
 library(ggplot2)
 library(readr)
 
-
+# import the CSV
 loandata <- read.csv('LoanStats3a.csv')
 
-failed_loan <- loandata %>%
+# create a subset of data limited to those loans that failed + features 
+loan <- loandata %>%
   filter(loan_status == "Charged Off") %>%
   select(loan_amnt,term,int_rate,installment,sub_grade,home_ownership,
          annual_inc,pymnt_plan,purpose,dti,total_pymnt,total_rec_prncp) %>%
   mutate(pct_paid = total_rec_prncp/loan_amnt * 100)
+
+#factor loan_term; 2 levels (3,5 years)
+loan$term <- factor(loan$term, labels = c("3year","5year"))
+
+# factor loan grades info @ https://www.lendingclub.com/foliofn/rateDetail.action
+loan$sub_grade <- factor(loan$sub_grade)
+summary(loan$sub_grade)
+
+# factor home_ownership
+loan$home_ownership <- factor(loan$home_ownership, levels = c("RENT","MORTGAGE","OWN"))
+
+
+unique(loan$purpose)
+# there are 15 purpose categories, -> factor 
+loan$purpose <- factor(loan$purpose)
+summary(loan$purpose)
+
+
+
 
 ggplot(failed_loan, aes(loan_amnt,total_pymnt)) +
   geom_point()
