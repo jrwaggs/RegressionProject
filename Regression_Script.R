19256088a129de866f4f33df5881286b6e05fedd
Waggoner <jr@waggs.net>
2019-04-08 16:18:42 -0500

    added a couple of new visualizations

--- Regression_Script.R
+++ Regression_Script.R
@@ -10,7 +10,7 @@ loandata <- read.csv('LoanStats3a.csv')
 loan <- loandata %>%
   filter(loan_status == "Charged Off") %>%
   select(loan_amnt,term,int_rate,inq_last_6mths,sub_grade,home_ownership,
-         annual_inc,pymnt_plan,purpose,dti,total_pymnt,total_rec_prncp) %>%
+         annual_inc,purpose,dti,total_pymnt,total_rec_prncp) %>%
   mutate(pct_paid = total_rec_prncp/loan_amnt * 100)
 
 #factor loan_term; 2 levels (3,5 years)
@@ -31,9 +31,15 @@ unique(loan$purpose)
 loan$purpose <- factor(loan$purpose)
 summary(loan$purpose)
 
+#------------------------------- data exploration -------------------------------------
+
+#distribution of loan amounts
+ggplot(loan,aes(loan_amnt))+
+  geom_histogram()
+
 
 # distribution of loans by % paid
-ggplot(failed_loan, aes(pct_paid))+
+ggplot(loan, aes(pct_paid))+
   geom_histogram()
 
 mean(loan$pct_paid) # the mean % paid back on a failed loan is 35.38%
@@ -44,5 +50,18 @@ ggplot(loan, aes(purpose, pct_paid)) +
   geom_boxplot() +
   coord_flip()
 
+# boxplot plot of loan grade vs.pct_paid
+ggplot(loan,aes(sub_grade,pct_paid))+
+  geom_boxplot()
 
+# scatterplot plot of loan grade vs.pct_paid
+ggplot(loan,aes(sub_grade,pct_paid))+
+  geom_point()
 
+# DTI distribution
+ggplot(loan, aes(dti))+
+  geom_histogram()
+
+# DTI vs. pct_paid
+ggplot(loan, aes(dti, pct_paid))+
+  geom_point()
