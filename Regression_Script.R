ed24714ba61ae04a5bd500328463f29adaa8f764
Waggoner <jr@waggs.net>
2019-04-10 16:17:57 -0500

    Added labels/titles to existing plots

--- Regression_Script.R
+++ Regression_Script.R
@@ -33,32 +33,42 @@ summary(loan$purpose)
 
 
 #------------------------------- data exploration -------------------------------------
+mean(loan$pct_paid) # the mean % paid back on a failed loan is 35.38%
 
 #distribution of loan amounts
 ggplot(loan,aes(loan_amnt))+
-  geom_histogram()
-
+  geom_histogram(bins = 7)+
+  ggtitle("Distribution of Loan Amounts")+
+  xlab("Loan amount")
 
 # distribution of loans by % paid
 ggplot(loan, aes(pct_paid))+
-  geom_histogram()
-
-mean(loan$pct_paid) # the mean % paid back on a failed loan is 35.38%
-
+  geom_histogram()+
+  ggtitle("Distribution of % Paid")+
+  xlab("% Paid")
 
 # boxplot of % repaid by purpose, with coordinates flipped
 ggplot(loan, aes(purpose, pct_paid))+
   geom_boxplot()+
-  coord_flip()
+  coord_flip()+
+  ggtitle("% Paid by Purpose")+
+  xlab("% Paid")+
+  ylab("Purpose")
 
 # boxplot of loan amount by purpose
 ggplot(loan,aes(purpose,loan_amnt))+
   geom_boxplot()+
-  coord_flip()
+  coord_flip()+
+  ggtitle("Loan Amount by Purpose")+
+  xlab("Purpose")+
+  ylab("Loan Amount")
 
 # boxplot plot of loan grade vs.pct_paid
 ggplot(loan,aes(sub_grade,pct_paid))+
-  geom_boxplot()
+  geom_boxplot()+
+  ggtitle("% Paid by Sub-grade")+
+  xlab("Loan Sub-Grade")+
+  ylab("% Paid")
 
 # scatterplot plot of loan grade vs.pct_paid
 ggplot(loan,aes(sub_grade,pct_paid))+
@@ -66,7 +76,9 @@ ggplot(loan,aes(sub_grade,pct_paid))+
 
 # DTI distribution
 ggplot(loan, aes(dti))+
-  geom_histogram()
+  geom_histogram()+
+  ggtitle("Distribution of Borrower DTI")+
+  xlab("DTI")
 
 # DTI vs. pct_paid
 ggplot(loan, aes(dti, pct_paid))+
@@ -74,4 +86,6 @@ ggplot(loan, aes(dti, pct_paid))+
 
 # distribution of interest rates
 ggplot(loan,aes(int_rate))+
-  geom_histogram()
+  geom_histogram(bins=20)+
+  ggtitle("Distribution of Loan Interest Rates")+
+  xlab("Interest Rate")
